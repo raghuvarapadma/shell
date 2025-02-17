dee4f2883e7ed575e141c69e904a4aad5e58ab8d
Raghuvara Padma <raghuvarapadma@gmail.com>
2025-02-17 15:49:42 -0500

    redirection pt 2

--- src/main.c
+++ src/main.c
@@ -13,8 +13,8 @@ void run_program();
 void parse_input();
 int fetch_line();
 int parse_command(char *stdin_input, int* write_end_pipe, int* read_end_pipe, int redirection_fd);
-int find_path(char* command, char **arguments, int* write_end_pipe, int* read_end_pipe);
-int execute_command(char *command_path, char **arguments, int* write_end_pipe, int* read_end_pipe);
+int find_path(char* command, char **arguments, int* write_end_pipe, int* read_end_pipe, int redirection_fd);
+int execute_command(char *command_path, char **arguments, int* write_end_pipe, int* read_end_pipe, int redirection_fd);
 void free_memory_fetch_line(char* stdin_string, int** file_descriptors, int command_counter);
 void free_memory_parse_command(char** arguments, int arguments_length, char* argument, char* command);
 void free_memory_find_path(char* path_result, char* search_path);
@@ -81,19 +81,18 @@ int fetch_line() {
 		}
 
 		if (redirection == 1) {
-			if (ch == '<') {
+			if (ch == '>') {
 				fprintf(stderr, "This shell doesn't support chained redirection right now!");
 				free_memory_fetch_line(stdin_string, file_descriptors, command_counter);
 				return 0;
 			} else if (ch == '\n' || ch == ';' || ch == '|') {
-				redirection = 0;
+				stdin_string[count_length_string] = '\0';
 				int handle_redirection_result = handle_redirection(stdin_string_redirection, stdin_string, file_descriptors, command_counter, command_counter_redirection);
 				if (handle_redirection_result == 1) {
 					fprintf(stderr, "handle_redirection_result() failed");
 					free_memory_fetch_line(stdin_string, file_descriptors, command_counter);
 					return 0;
 				}
-				return 0;
 				// TODO: Handle memory for stdin_string_redirection.
 				// What to do after running redirection in the cases above? Do
 				// I just let everything run as usually? Clear memory. Need to
@@ -104,16 +103,17 @@ int fetch_line() {
 
 		if (ch == '\n') {
 			stdin_string[count_length_string] = '\0';
-			if (call_parse_command('\n', stdin_string, file_descriptors, command_counter) == 1) {
+			if (redirection == 0 && call_parse_command('\n', stdin_string, file_descriptors, command_counter) == 1) {
 				return 0;
 			}
 			free_memory_fetch_line(stdin_string, file_descriptors, command_counter);
 			return 0;
 		} else if (ch == ';') {
 			stdin_string[count_length_string] = '\0';
-			if (call_parse_command(';', stdin_string, file_descriptors, command_counter) == 1) {
+			if (redirection == 0 && call_parse_command(';', stdin_string, file_descriptors, command_counter) == 1) {
 				return 0;
 			}
+			redirection = 0;
 			count_length_string = 0;
 			command_counter++;
 		} else if (ch == '|') {
@@ -129,9 +129,17 @@ int fetch_line() {
 				return 0;
 			}
 			stdin_string[count_length_string] = '\0';
-			if (call_parse_command('|', stdin_string, file_descriptors, command_counter) == 1) {
-				return 0;
+			if (redirection == 0) {
+				if (call_parse_command('|', stdin_string, file_descriptors, command_counter) == 1) {
+					return 0;
+				}
+			} else if (redirection == 1) {
+				if (close_pipe_end(file_descriptors[command_counter][1]) == 1) {
+					free_memory_fetch_line(stdin_string, file_descriptors, command_counter);
+					return 0;
+				}
 			}
+			redirection = 0;
 			count_length_string = 0;
 			command_counter++;
 		} else if (ch == '>') {
@@ -139,10 +147,10 @@ int fetch_line() {
 			command_counter_redirection = command_counter;
 			stdin_string_redirection = (char*)malloc(strlen(stdin_string)*sizeof(char));
 			strcpy(stdin_string_redirection, stdin_string);
+			memset(stdin_string, 0, BUFFER_SIZE);
 			redirection = 1;
 			count_length_string = 0;
 			command_counter++;
-			printf("%s", "at >\n");
 		} else {
 			stdin_string[count_length_string] = ch;
 			count_length_string++;
@@ -257,10 +265,17 @@ int parse_command(char *stdin_input, int* write_end_pipe, int* read_end_pipe, in
 			}
 		}
 
-		if (write_end_pipe != NULL) {
+		if (write_end_pipe != NULL || redirection_fd != -1) {
+			int write_to_pipe;
+			if (redirection_fd != -1) {
+				write_to_pipe = redirection_fd;
+			} else {
+				write_to_pipe = write_end_pipe[1];
+			}
+
 			for (int i = 1; i < argument_index; i++) {
 				strcat(arguments[i], " ");
-				if (write(write_end_pipe[1], arguments[i], strlen(arguments[i])) == -1) {
+				if (write(write_to_pipe, arguments[i], strlen(arguments[i])) == -1) {
 					free_memory_parse_command(arguments, argument_index, argument, command);
 					return 1;
 				}
@@ -288,8 +303,15 @@ int parse_command(char *stdin_input, int* write_end_pipe, int* read_end_pipe, in
 
 		char cwd[PATH_MAX];
 		if (getcwd(cwd, sizeof(cwd)) != NULL) {
-			if (write_end_pipe != NULL) {
-				if (write(write_end_pipe[1], cwd, strlen(cwd)) == -1) {
+			if (write_end_pipe != NULL || redirection_fd != -1) {
+				int write_to_pipe;
+				if (redirection_fd != -1) {
+					write_to_pipe = redirection_fd;
+				} else {
+					write_to_pipe = write_end_pipe[1];
+				}
+
+				if (write(write_to_pipe, cwd, strlen(cwd)) == -1) {
 					free_memory_parse_command(arguments, argument_index, argument, command);
 					return 1;
 				}
@@ -311,7 +333,7 @@ int parse_command(char *stdin_input, int* write_end_pipe, int* read_end_pipe, in
 		free_memory_parse_command(arguments, argument_index, argument, command);
 		return 0;
 	} else {
-		int find_path_result = find_path(command, arguments, write_end_pipe, read_end_pipe);
+		int find_path_result = find_path(command, arguments, write_end_pipe, read_end_pipe, redirection_fd);
 		free_memory_parse_command(arguments, argument_index, argument, command);
 		if (find_path_result == 1) {
 			fprintf(stderr, "Call to find_path() failed\n");
@@ -321,7 +343,7 @@ int parse_command(char *stdin_input, int* write_end_pipe, int* read_end_pipe, in
 	}
 }
 
-int find_path(char *command, char **arguments, int* write_end_pipe, int* read_end_pipe) {
+int find_path(char *command, char **arguments, int* write_end_pipe, int* read_end_pipe, int redirection_fd) {
 	char *path = "PATH";
 
 	int command_len = strlen(command);
@@ -372,7 +394,7 @@ int find_path(char *command, char **arguments, int* write_end_pipe, int* read_en
 		free_memory_find_path(path_result, search_path);
 		return 0;
 	} else {
-		int execute_command_result = execute_command(search_path, arguments, write_end_pipe, read_end_pipe);	
+		int execute_command_result = execute_command(search_path, arguments, write_end_pipe, read_end_pipe, redirection_fd);	
 		free_memory_find_path(path_result, search_path);
 		if (execute_command_result == 1) {
 			fprintf(stderr, "execute_command() call failed\n");
@@ -382,7 +404,7 @@ int find_path(char *command, char **arguments, int* write_end_pipe, int* read_en
 	}
 }
 
-int execute_command(char *command_path, char **arguments, int* write_end_pipe, int* read_end_pipe) {
+int execute_command(char *command_path, char **arguments, int* write_end_pipe, int* read_end_pipe, int redirection_fd) {
 	pid_t pid = fork();
 
 	if (pid < 0) {
@@ -394,21 +416,39 @@ int execute_command(char *command_path, char **arguments, int* write_end_pipe, i
 		int dup2_read_end;
 
 
-		if (write_end_pipe != NULL) {
+		if (redirection_fd != -1) {
+			if (write_end_pipe != NULL) {
+				if (close_pipe_end(write_end_pipe[0]) == 1) {
+					err(98, "close() error");
+				}
+				if (close_pipe_end(write_end_pipe[1]) == 1) {
+					err(98, "close() error");
+				}
+			}
+
+			dup2_write_end = dup2(redirection_fd, STDOUT_FILENO);
+			if (dup2_write_end == -1) {
+				err(EXIT_FAILURE, "dup2() command failed for redirection to STDOUT_FILENO");
+			}
+
+			if (close_pipe_end(redirection_fd) == 1) {
+				err(98, "close() error");
+			}
+		} else if (write_end_pipe != NULL) {
 			if (close_pipe_end(write_end_pipe[0]) == 1) {
 				err(98, "close() error");
 			}
 
 			dup2_write_end = dup2(write_end_pipe[1], STDOUT_FILENO);
 			if (dup2_write_end == -1) {
-				err(EXIT_FAILURE, "dup2() command failed for redirection to STDIN_FILENO");
+				err(EXIT_FAILURE, "dup2() command failed for redirection to STDOUT_FILENO");
 			}
 
 			if (close_pipe_end(write_end_pipe[1]) == 1) {
 				err(98, "close() error");
 			}
 		}
-
+		
 		if (read_end_pipe != NULL) {
 			if (close_pipe_end(read_end_pipe[1]) == 1) {
 				err(98, "close() error");
@@ -423,7 +463,7 @@ int execute_command(char *command_path, char **arguments, int* write_end_pipe, i
 				err(98, "close() error");
 			}
 		}
-		
+
 		execv(command_path, arguments);
 
 		err(99, "execv() failed in child process");
@@ -544,33 +584,32 @@ int handle_redirection(char* stdin_string_command, char* stdin_string_file, int*
 	// Pass in the stdin_string_command, with NULL to the pipes, and
 	// stdin_string_file to parse_command
 	// Modify parse_command, find_path, and execute (using dup2)
-	
-	while (isspace((char)*stdin_string_file)) {
-		stdin_string_file++;
-	}
-	
+
 	char *end = stdin_string_file + strlen(stdin_string_file)-1;
 	while (end > stdin_string_file && isspace((char)*end)) {
 		end--;
 	}
-
 	*(end+1) = '\0';
-	printf("%s\n", stdin_string_file);
+
+	while (isspace((char)*stdin_string_file)) {
+		stdin_string_file++;
+	}
+
 	int fd;
-	
+
 	fd = open(stdin_string_file, O_WRONLY | O_CREAT | O_TRUNC, 0755);
 	if (fd == -1) {
 		fprintf(stderr, "open() error while opening%s\n", stdin_string_file);
 		return 1;
 	}
-
-	int parse_command_result = parse_command(stdin_string_command, NULL, file_descriptors[command_counter_redirection], fd);
+	int parse_command_result = parse_command(stdin_string_command, file_descriptors[command_counter], file_descriptors[command_counter_redirection-1], fd);
 	if (parse_command_result == 1) {
 		fprintf(stderr, "parse_command() failed\n");
 		free_memory_fetch_line(stdin_string_command, file_descriptors, command_counter);
 		return 1;
 	}
 
+	close(fd);
 	return 0;	
 }
 
